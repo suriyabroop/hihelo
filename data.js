f46b74a81976f6778c33e91034e4a6aa5e88d91c
suriyabroop <98744069+suriyabroop@users.noreply.github.com>
2022-03-08 07:44:48 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://airdropbot:Kht8c3Fw74ZUCBSl@cluster0.bqqkj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+    
+    bot_token:'1894447010:AAGVj5pT9gQo0V7NF475c8hBC6avQO2VUlg',//bot token
+    
+    bot_name:'COINBASEdemobot',//bot username without @
+    
+    bot_admin:954467596,// admin telegram id
+    
+    channelsList : ['@demonodechanel'],// channels list
+    
+    reffer_bonus:0.00000004,// refferal bonus amount
+    
+    min_wd:0.00000004,//min withdrawal amount
+    
+    daily_bonus:0.0000004,// daily bonus amount
+    
+    currency:'LTC', //bot cuurency
+    
+    cb_api_key:'vo9JIRTDbPQnf9TO',//coinbase api key
+    
+    cb_api_secret:'iBRWs8ERRWuFpfV8bagxBgpbFmaFJUSz',// coinbase secret key
+    
+    cb_account_id:'f123b4d3-15ab-5852-86a3-8eaa796b612b',// counbase account id for the bot currency
+    
+    payment_channel:'@demonodechanel'// payment channel username
+    
+    }
